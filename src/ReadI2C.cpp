cbcb6d69a889776758c28cae99b08d6eea2b0f22
OksarD <odurham2003@gmail.com>
2024-04-24 17:17:05 +0930

    idk

--- src/ReadI2C.cpp
+++ src/ReadI2C.cpp
@@ -7,7 +7,6 @@ MPU6050 mpu;
 
 #define INTERRUPT_PIN 23 
 #define LED_PIN 2
-#define AD0_PIN 22
 #define DATA_PIN 21
 
 bool blinkState = false;
@@ -18,7 +17,10 @@ VectorInt16 aa;         // [x, y, z]            accel sensor measurements
 VectorInt16 aaReal;     // [x, y, z]            gravity-free accel sensor measurements
 VectorFloat gravity;    // [x, y, z]            gravity vector
 float ypr[3];           // [yaw, pitch, roll]   yaw/pitch/roll container and gravity vector
-extern short rcAnalogs[5]; 
+extern short rcAnalogs[4]; 
+extern bool ch3State = 0;
+extern bool ch4State = 0;
+extern byte ch7State = 0;
 
 // MPU control/status vars
 bool dmpReady = false;  // set true if DMP init was successful
@@ -28,10 +30,6 @@ uint16_t packetSize;    // expected DMP packet size (default is 42 bytes)
 uint16_t fifoCount;     // count of all bytes currently in FIFO
 uint8_t fifoBuffer[64]; // FIFO storage buffer
 uint8_t rcBuffer[9];   // RC storage buffer
-extern bool ch3State = 0;
-extern bool ch4State = 0;
-extern byte ch7State = 0;
-
 
 volatile bool mpuInterrupt = false;     // indicates whether MPU interrupt pin has gone high
 void dmpDataReady() {
@@ -106,7 +104,6 @@ void I2Csetup() {
 }
 
 void I2Cloop() {
-    digitalWrite(AD0_PIN, HIGH);
     Wire.requestFrom(0x60, 9);
     for (byte i = 0; i < 9; i++) {
         while(!Wire.available());
@@ -122,7 +119,6 @@ void I2Cloop() {
     // if programming failed, don't try to do anything
     if (!dmpReady) return;
     // read a packet from FIFO
-    digitalWrite(AD0_PIN, LOW);
     if (mpu.dmpGetCurrentFIFOPacket(fifoBuffer)) { // Get the Latest packet 
 
         #ifdef OUTPUT_READABLE_YAWPITCHROLL
